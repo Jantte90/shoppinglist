5f15d9aebae15e5adc6cc75630cf7a783fbca8d8
Jantte90 <janne.roman@gmail.com>
2022-12-11 12:25:27 -0800

    sdfsdf

--- admin.php
+++ admin.php
@@ -1,46 +1,78 @@
 <?php
 require('./include/header.php');
-require_once('./include/functions.php');
+require_once 'include/functions.php';
+
 
 $db = openSQLite();
 
 session_start();
 
+
 if (isset($_SESSION["username"])) {
 
-    $getAdminLevel = "select level from ADMIN where (username = '" . $_SESSION["username"] . "')";
-    $getAdminLevel = selectAsJson($db, $getAdminLevel);
+    $getAdminlevel = "select level from ADMIN where (username = '" . $_SESSION["username"] . "')";
+    $getAdminlevel = selectAsJson($db, $getAdminlevel);
 
-    if (count($getAdminLevel) > 0) {
-        //jos tarvitaan adminille eri tasoja, voidaan määrittää mitä voivat tehdä milläkin tasolla esim 1-3
-        echo "Admin level: " . $getAdminLevel[0]["level"] . PHP_EOL;
+
+    if (count($getAdminlevel) > 0) {
+
+        if (!isset($_GET["action"])) {
+            http_response_code(400);
+            echo('no action');
+            return;
+        }
+
 
         if (isset($_GET["action"])) {
             $action = $_GET["action"];
 
-            switch ($action) {
-
-                case "additem":
-
-                    $user = $_SESSION["username"];
+            switch($action) {
+                case "addTuote":
+                    $id = $_POST['id'];
+                    $username = $_POST['username'];
                     $tuote = $_POST['tuote'];
 
-                    $sql = "INSERT INTO ostoskori (username, tuote) VALUES ('$user', '$tuote'); ";
+                    $sql = "INSERT INTO ostoslista (id,username, tuote) VALUES ('$id','$username', '$tuote'); ";
 
                     try {
                         executeInsert($db, $sql);
-                        echo json_encode(['Tuote onnistuneesti lisätty ostoslistaan', true, 'itemAdded']);
+                        echo json_encode('Tuote onnistuneesti lisätty ostoslistaan');
                     } catch (PDOException $pdoex) {
                         returnError($pdoex);
                         echo "Failed";
                     }
+                break;
+
+                case "removeTuote":
+                    $id = $_POST['id'];
+
+                    $sql = "DELETE FROM ostoslista WHERE id = '$id' ";
+
+                    try {
+                        executeInsert($db, $sql);
+                        echo json_encode('Tuote ' . $id ."  poistettu onnistuneesti ostoslistasta");
+                    } catch (PDOException $pdoex) {
+                        returnError($pdoex);
+                        echo "Failed";
+                    }
+                break;
+             
+                case "getLevel":
+                    http_response_code(200);
+                    echo json_encode(['User is an admin', true, 'isAdmin', $getAdminlevel]);
+                    return;
+                break;
             }
-        }
+        }   
     } else {
         http_response_code(401);
-        echo "Ei lupaa. Ainoastaan admin saa lisätä ja poistaa ostoslistasta.";
-    }
-} else {
+        echo "Et ole admin, tarvitset admin oikeudet.";
+        return;
+    }  
+} else { 
     http_response_code(401);
-    echo "Ei lupaa. Ei olemassaolevaa sessiodataa. Kirjaudu sisälle.";
-}
+    echo "Et ole kirjautunut sisään.";
+    return;
+}       
+                    
+?>
